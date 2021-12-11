73f4e2ffc19a489fad508a21c89f0857cd57f3bd
Lavatsa Leon <lavatsaleo@gmail.com>
2021-12-12 01:38:10 +0300

    add html and php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-hello world!!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAVATSA ESTATE</title>
+</head>
+<body>
+    <h1 style="color:red;">WELCOME TO LAVATSA ESTATES</h1>
+    <P>Welcome to Lavatsa Estate official tenant portal</P>
+</body>
+</html>
